0efaa92e45e85499511e31ea937fc2cc04dcbd6b
Steven Valladares <stevenfvalladares@gmail.com>
2023-02-07 16:30:14 -0300

    The function to test the GET/cafes route is added

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,5 +2,15 @@ const request = require("supertest");
 const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
+  it("Obteniendo un 200", async () => {
+    const response = await request(server).get("/cafes").send();
+    const status = response.statusCode;
+    expect(status).toBe(200);
+  });
 
+  it("El tipo de dato obtenido es un arreglo con un objeto", async () => {
+    const { body } = await request(server).get("/cafes/1").send();
+    const arrayObject = body;
+    expect(arrayObject).toBeInstanceOf(Object);
+  });
 });
