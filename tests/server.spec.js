37ddbf5996cc26a9d1fe74c4e4c5d3457ee06feb
Steven Valladares <stevenfvalladares@gmail.com>
2023-02-07 21:40:22 -0300

    The function to test the post method when adding a new cafe is added

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -25,4 +25,14 @@ describe("Operaciones CRUD de cafes", () => {
     const status = response.statusCode;
     expect(status).toBe(404);
   });
+
+  it("Obteniendo un 201 al agregar un nuevo tipo de cafe", async () => {
+    const id = 5;
+    const item = { id, nombre: "Espresso" };
+    const response = await request(server).post("/cafes").send(item);
+    const items = response.body;
+    const status = response.statusCode;
+    expect(status).toBe(201);
+    expect(items).toContainEqual(item);
+  });
 });
